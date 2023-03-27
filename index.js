9fbaaf6272c5c927bb69f89f62c9948f6537b4b2
tonantr <tonantruong@hotmail.com>
2023-03-27 18:19:01 -0400

    complete Array Filter Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter((drivers) => {return drivers.toLowerCase() === string.toLowerCase()});
+};
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter((drivers) => {return drivers.toLowerCase().substring(0, string.length) === string.toLowerCase()});
+};
+
+
+function matchName(drivers, string) {
+    return drivers.filter((drivers) => {return drivers.name === string});
+};
